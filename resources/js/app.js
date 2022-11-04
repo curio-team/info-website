ff3241a7c555c7bcb8ff272b769b1caa282a1f39
Luttje <2738114+luttje@users.noreply.github.com>
2022-11-04 14:15:10 +0100

    tweak ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const animatedEl = document.getElementById('animatedEl');
+    const elsToModify = document.querySelectorAll('[data-add-class-after-intro]');
+
+    elsToModify.forEach(el => {
+        if(el.dataset.noIntro !== undefined)
+            el.classList.add(el.dataset.addClassAfterIntro);
+    });
+        
+
+    animatedEl.addEventListener("animationend", function() {
+        elsToModify.forEach(function(el) {
+            el.classList.add(el.dataset.addClassAfterIntro);
+        });
+    });
+});
