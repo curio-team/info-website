fa32623f5ce2ed1dd987b75fcbbb4a663b496e9c
luttje <2738114+luttje@users.noreply.github.com>
2022-12-24 08:49:39 +0100

    add logo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm p-2">
     <div class="container">
 
-        <a class="navbar-brand text-primary font-weight-bold text-uppercase" href="{{ url('/') }}">
-            SD Info Website
+        <a class="max-w-[200px]" href="{{ url('/') }}">
+            <x-logos.light-logo />
         </a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
