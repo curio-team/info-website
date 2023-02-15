b2faaa39159dbbec3e7ea6cd8c1de5b95d1ce397
Steven Boeren <70892306+stevenboeren@users.noreply.github.com>
2023-02-15 10:09:33 +0100

    Fixed btn-height in sites index

--- resources/views/app/sites/index.blade.php
+++ resources/views/app/sites/index.blade.php
@@ -25,7 +25,7 @@
                 </div>
                 <div class="grow">
                     @can('create', App\Models\Site::class)
-                    <x-buttons.primary href="{{ route('sites.create') }}">
+                    <x-buttons.primary class="btn-full-height" href="{{ route('sites.create') }}">
                         @lang('crud.common.create')
                     </x-buttons.primary>
                     @endcan
