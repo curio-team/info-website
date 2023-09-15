1388412dd6e2fd6a983d0e8b6c0d071f59d1121e
NitWitted <totallynitwitted@gmail.com>
2023-09-15 09:53:42 +0200

    Added flags to test page

--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -42,9 +42,45 @@
             </span>
         </small>
     </h2>
-    @sectionMissing('is_test')
+    @hasSection('is_test')
     <div class="i18n">
         <div class="flags @unless(isset($_GET['noanim'])) delay-animate @endunless">
+            <a title="Bekijk een Nederlandstalige versie van onze site (gemaakt door een van onze studenten)">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     id="flag-icons-nl"
+                     viewBox="0 0 640 480"
+                     class="shadow">
+                    <path fill="#21468b"
+                          d="M0 0h640v480H0z" />
+                    <path fill="#fff"
+                          d="M0 0h640v320H0z" />
+                    <path fill="#ae1c28"
+                          d="M0 0h640v160H0z" />
+                </svg>
+            </a>
+            <a title="View an English version of our site (made by one of our students)">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     id="flag-icons-gb"
+                     viewBox="0 0 640 480"
+                     class="shadow">
+                    <path fill="#012169"
+                          d="M0 0h640v480H0z" />
+                    <path fill="#FFF"
+                          d="m75 0 244 181L562 0h78v62L400 241l240 178v61h-80L320 301 81 480H0v-60l239-178L0 64V0h75z" />
+                    <path fill="#C8102E"
+                          d="m424 281 216 159v40L369 281h55zm-184 20 6 35L54 480H0l240-179zM640 0v3L391 191l2-44L590 0h50zM0 0l239 176h-60L0 42V0z" />
+                    <path fill="#FFF"
+                          d="M241 0v480h160V0H241zM0 160v160h640V160H0z" />
+                    <path fill="#C8102E"
+                          d="M0 193v96h640v-96H0zM273 0v480h96V0h-96z" />
+                </svg>
+            </a>
+        </div>
+    </div>
+    @else
+    <div class="i18n">
+        <div
+            class="flags @unless(isset($_GET['noanim'])) delay-animate @endunless">
             <a href="{{ route('home') }}"
                title="Bekijk een Nederlandstalige versie van onze site (gemaakt door een van onze studenten)">
                 <svg xmlns="http://www.w3.org/2000/svg"
